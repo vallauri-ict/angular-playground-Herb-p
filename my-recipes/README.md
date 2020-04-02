c3791adbf65d9d36d9ec8872a41089f68ef658a9
Alberto Piola <a.piola.0509@vallauri.edu>
2020-04-02 20:27:48 +0200

    Lezione 3

--- my-recipes/README.md
+++ my-recipes/README.md
@@ -4,9 +4,29 @@ Al progetto è stata linkata la libreria Bootstrap v. 3.4.1
 
 ## Lezione 1
 ### Obiettivi
-- creare i
+- creare i componenti e testare il loro funzionamento
 ### Componenti
+- recipes
+- recipe-detail
+- recipe-list
+- recipe-item
+- shopping-list
+- shopping-edit
 #### 
 
 ## Lezione 2
 ### Obiettivi
+- sviluppare i seguenti componenti precedentemente creati in modo da inserirli efficaciemente nel funzionamento del sito.
+### Componenti
+- recipes
+- shopping-list
+#### 
+
+## Lezione 3
+### Obiettivi
+- sviluppare i seguenti componenti precedentemente creati in modo da inserirli efficaciemente nel funzionamento del sito.
+### Componenti
+- recipe-detail
+- recipe-list
+- recipe-item
+ 
--- my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[]=[
+  new Recipe('A Test Recipe','This is simply a test','https://aws.imagelinenetwork.com/agronotizie/materiali/ArticoliImg/agroalimentare-cibo-dieta-mediterranea-alimentazione-by-denio109-fotolia-750.jpeg'),
+  new Recipe('','',''),
+  new Recipe('','',''),
+  new Recipe('','',''),
+  new Recipe('','',''),
+  new Recipe('','',''),
+  new Recipe('','',''),
+  new Recipe('','',''),
+  new Recipe('','','')
+  ]
 
   constructor() { }
 
